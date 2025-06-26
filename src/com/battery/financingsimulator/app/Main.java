f137d5f3e54714d3a8480bd4ec094da29bda9c03
Mateus Santos <mateus.santos@landisgyr.com>
2025-06-25 21:21:04 -0300

    Update Main to support recent changes

--- src/com/battery/financingsimulator/app/Main.java
+++ src/com/battery/financingsimulator/app/Main.java
@@ -1,7 +1,26 @@
 package com.battery.financingsimulator.app;
 
+import com.battery.financingsimulator.model.*;
+import com.battery.financingsimulator.ui.UserInputHandler;
+
+/**
+ * The main entry point for the Financing Simulator application.
+ */
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		System.out.println("### Welcome to the Financing Simulator ###");
+
+		UserInputHandler inputHandler = new UserInputHandler();
+
+		double propertyValue = inputHandler.askPropertyValue();
+		int loanTerm = inputHandler.askLoanTerm();
+		double interestRate = inputHandler.askInterestRate();
+
+		Financing financing = new Financing(propertyValue, loanTerm, interestRate);
+
+		System.out.println("\n--- FINANCING DETAILS ---");
+		System.out.println(financing);
+
+		inputHandler.closeScanner();
 	}
 }
